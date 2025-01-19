13d295614ea2494f4c1b8c856005a5440110dc32
pedram-aliniaye-asli <pedram.aliniayeasli@gmail.com>
2025-01-19 17:46:01 -0500

    Solved the issue of directories with same name.

--- lib/icd/icd.py
+++ lib/icd/icd.py
@@ -27,19 +27,18 @@ def handle_ctrl_c(signum, frame):
 signal.signal(signal.SIGINT, handle_ctrl_c)
 
 # Function to retrieve the list of directories in a given path
-def get_dir_list(address,dir):
-    if not dir:
+def get_dir_list(address, dir_index):
+    if dir_index == 0:
         dir = '/'
     else:
-        dir = "/".join(address[:address.index(dir)+1]) # Construct the full directory path
+        dir = "/".join(address[:dir_index+1])
     dirs = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))] # List only directories
     return dirs
 
 # Function to print the current working directory in a user-friendly format
 def print_working_directory():
-    last_output = os.getcwd()
-    print(last_output[:last_output.find(os.path.basename(last_output))] + f'[{os.path.basename(last_output)}]', end="", flush=True)
-    return last_output
+    current_dir = os.getcwd()
+    print(current_dir[:current_dir.find(os.path.basename(current_dir))] + f'[{os.path.basename(current_dir)}]', end="", flush=True)
 
 # Function to read a single key press from the user (without blocking program)
 def get_key():
@@ -77,28 +76,28 @@ def wait_for_key_presses():
             return 'Enter'
 
 # Function to handle user input and update the directory navigation based on that input       
-def check_input(key_input, address, depth_point, current_loc = '.'):
+def check_input(key_input, address, depth_point, current_loc_index):
     global working_add
-    dirs_list = get_dir_list(address, current_loc)
+    dirs_list = get_dir_list(address, current_loc_index)
     final_result = None
     if key_input == 'R': # Move right
         if depth_point > 0:
-            new_current_loc = get_dir_list(address, current_loc)[depth_point-1]
-            address = address[:address.index(current_loc)+1]
+            new_current_loc = get_dir_list(address, current_loc_index)[depth_point-1]
+            address = address[:current_loc_index+1]
             address.append(new_current_loc)
-            current_loc = new_current_loc
+            current_loc_index += 1
         elif depth_point == 0:
-            if address.index(current_loc) < len(address) - 1:
-                current_loc = address[address.index(current_loc) + 1]
-        dirs_list = get_dir_list(address, current_loc)
+            if current_loc_index < len(address) - 1:
+                current_loc_index += 1
+        dirs_list = get_dir_list(address, current_loc_index)
         depth_point = 0
     elif key_input == 'L': # Move left
         address_line = "/".join(address)
-        if address.index(current_loc) > 0:
-            current_loc = address[address.index(current_loc) - 1]
+        if current_loc_index > 0:
+            current_loc_index -= 1
         if len(address_line) >= shutil.get_terminal_size().columns:
-            address = address[:address.index(current_loc)+1]
-        dirs_list = get_dir_list(address, current_loc)
+            address = address[:current_loc_index+1]
+        dirs_list = get_dir_list(address, current_loc_index)
         depth_point = 0
     dirs_list.insert(0, '.') # Add the current directory (.) to the list
     if key_input == 'U': # Move up
@@ -112,15 +111,15 @@ def check_input(key_input, address, depth_point, current_loc = '.'):
         else:
             depth_point += 1
     if key_input == 'Enter': # Enter a directory
-        final_result = change_directory(address, current_loc, dirs_list, depth_point)
-
+        final_result = change_directory(address, current_loc_index, dirs_list, depth_point)
     working_add = address
-    return (dirs_list, current_loc, depth_point, final_result)
+    return (dirs_list, current_loc_index, depth_point, final_result)
 
 # Function to print the current directory path with visual indicators
-def print_path_string(address,current_dir, next_list, depth_point, input_list):
+def print_path_string(address,current_dir_index, next_list, depth_point, input_list):
     global lines_to_delete
-    path_string = "/".join(address) # Build the full path string
+    current_dir = address[current_dir_index]
+    path_string = ("/".join(address[:current_dir_index]) + f'/[{address[current_dir_index]}]/' + "/".join(address[current_dir_index+1:])).rstrip('/') # Build the full path string
     extra_chars = len(path_string) - shutil.get_terminal_size().columns 
     if extra_chars > 0: # Check if the path is too long for the terminal
         path_string = '...' + path_string[-(2*extra_chars):] # Truncate the path if it's too long
@@ -128,15 +127,15 @@ def print_path_string(address,current_dir, next_list, depth_point, input_list):
         delete_lines(input_list) # Delete previous output lines
     else:
         input_list[0] = True
-    print(path_string[:path_string.find(current_dir)] + f'[{current_dir}]' + path_string[path_string.find(current_dir)+len(current_dir):], end="\n", flush=True)
+    print(path_string, end="\n", flush=True)
     for index,item in enumerate(next_list): # Print directories in the current folder
         print(f'[{"*" if index == depth_point else " "}]',item, end="\n", flush=True)
     lines_to_delete[1] = address
     lines_to_delete[2] = next_list
 
 # Function to change the current directory based on user input
-def change_directory(working_add, current_dir, dirs_list, depth_point):
-    final_add = "/".join(working_add[:working_add.index(current_dir)]) + f'/{current_dir}' + f'/{dirs_list[depth_point]}'
+def change_directory(working_add, current_dir_index, dirs_list, depth_point):
+    final_add = "/".join(working_add[:current_dir_index]) + f'/{working_add[current_dir_index]}' + f'/{dirs_list[depth_point]}'
     if os.path.isdir(final_add): # If the directory exists, returns it
         return final_add
     return None
@@ -144,18 +143,6 @@ def change_directory(working_add, current_dir, dirs_list, depth_point):
 # Function to delete previous lines in the terminal to keep the output clean
 def delete_lines(input_list):
     terminal_width = shutil.get_terminal_size().columns
-    # max_length = 0
-    # for line in input_list[2]:
-    #     if max_length < len(line):
-    #         max_length = len(line)
-    # for line in input_list[2]:
-    #     print('\r', end="")
-    #     print("    " * max_length, end = " ")
-    #     print('\033[F', end="")
-    # print('\033[F', end="")
-    # first_line = "/".join(input_list[1])
-    # print(" " * len(first_line), end = " ")
-    # print("\r", end="")
     for line in input_list[2]: # Loop through the lines to delete
         print('\r', end="") # Return to the starting position
         print(" " * (terminal_width - 2), end = " ") # Clear the line
@@ -174,18 +161,17 @@ def main():
     global working_add
     working_add = os.getcwd().split('/') # Convert the current working directory into a list of folders
     current_dir = os.path.basename(os.getcwd()) # Get the name of the current directory
+    current_dir_index = working_add.index(current_dir)
     global lines_to_delete
     depth_point = 0 # Start at the top of the directory list
     while True:
         key_input = wait_for_key_presses() # Wait for user input
-
-        dirs = check_input(key_input, working_add, depth_point, current_dir) # Process the input
+        dirs = check_input(key_input, working_add, depth_point, current_dir_index) # Process the input
         if dirs[3]: # If a new directory is selected, change to it
             print(dirs[3])
             break
         print_path_string(working_add, dirs[1], dirs[0], dirs[2], lines_to_delete) # Update the path display
-        current_dir = dirs[1] # Set the new current directory
         depth_point = dirs[2] # Update the depth point
-
+        current_dir_index = dirs[1] # Update the current directory index
 if __name__ == "__main__":
     main()
